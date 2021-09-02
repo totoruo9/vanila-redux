2339ee551999a2df09feab67e1f5c8340f93f4e0
totoruo9 <55140745+totoruo9@users.noreply.github.com>
2021-09-02 16:50:17 +0900

    [Vanilla Redux] self make Todo List-1 (bug)

--- src/index.js
+++ src/index.js
@@ -1,39 +1,58 @@
 import {createStore} from "redux";
 
-const add = document.getElementById("add");
-const minus = document.getElementById("minus");
-const number = document.querySelector("span");
-
-number.innerText = 0;
+const input = document.querySelector("input");
+const button = document.querySelector("button");
+const ul = document.querySelector("ul");
 
 const ADD = "add";
-const MINUS = "minus"
 
-const countModifier = (count = 0, action) => {
+let todoList = [];
+
+const reducer = (todoList, action, a) => {
+  console.log(a);
   switch(action.type){
     case ADD:
-      return ++count;
-    case MINUS:
-      return --count;
+      return todoList.concat(action.payload);
     default:
-      return count;
+      return todoList;
   }
-};
-
-const countStore = createStore(countModifier);
-
-const onChange = () => {
-  number.innerText = countStore.getState();
 }
 
-countStore.subscribe(onChange);
+const todoListStore = createStore(reducer);
 
-const handleAdd = () => {
-  countStore.dispatch({type:ADD});
-}
-const handleMiuns = () => {
-  countStore.dispatch({type:MINUS});
+
+todoListStore.replaceReducer((a,b) => {
+  console.log(a, b);
+});
+
+todoListStore.dispatch({type:ADD, payload:"hellos"});
+
+
+const listenSubScribe = () => {
+  ul.innerHTML = todoList.map(todo => {
+    console.log(todo)
+    console.log(todoList);
+    return <li>{todo.payload}</li>
+  });
+  console.log(todoListStore.getState())
 }
 
-add.addEventListener("click",handleAdd);
-minus.addEventListener("click",handleMiuns);
+todoListStore.subscribe(listenSubScribe);
+
+let inputText = "";
+
+const handleChange = (event) => {
+  const {
+    target: {value}
+  } = event;
+  inputText = value;
+}
+
+const handleSubmit = (event) => {
+  event.preventDefault();
+  todoListStore.dispatch({type: ADD, payload: inputText});
+}
+
+input.addEventListener("change", handleChange);
+button.addEventListener("click", handleSubmit);
+
