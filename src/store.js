e2ec6f94c66a46a31450cd611a1a7d8445ee8a46
totoruo9 <55140745+totoruo9@users.noreply.github.com>
2021-09-03 16:11:08 +0900

    [Redux toolkit] use configureStore & redux DevTools

--- src/store.js
+++ src/store.js
@@ -1,30 +1,8 @@
 import {createStore} from "redux";
-import {createAction, createReducer} from "@reduxjs/toolkit";
+import {configureStore, createAction, createReducer} from "@reduxjs/toolkit";
 
 const addToDo = createAction("ADD");
 const deleteToDo = createAction("DELETE");
-
-// const reducer = (state = [], action) => {
-//     if(localStorage.length === 0){
-//         localStorage.setItem("toDos", JSON.stringify([]));
-//     }
-//     switch(action.type){
-//         case addToDo.type:
-//             const getStorageToDo = JSON.parse(localStorage.getItem("toDos"));
-//             const addStorage = getStorageToDo.concat({text: action.payload, id: Date.now()});
-//             localStorage.setItem("toDos", JSON.stringify(addStorage));
-
-//             return [{text: action.payload, id: Date.now()}, ...state];
-//         case deleteToDo.type:
-//             const todos = JSON.parse(localStorage.getItem("toDos"));
-//             const delStorage = todos.filter(todo => todo.id !== action.payload);
-//             localStorage.setItem("toDos", JSON.stringify(delStorage));
-            
-//             return state.filter(toDo => toDo.id !== action.payload);
-//         default:
-//             return state;
-//     };
-// };
 
 const reducer = createReducer([], {
     [addToDo]: (state, action) => {
@@ -41,10 +19,10 @@ const reducer = createReducer([], {
     }
 })
 
-const store = createStore(reducer);
+const store = configureStore({reducer});
 
 export const actionCreatetors = {
     addToDo,
     deleteToDo
-}
+}                     
 export default store;
